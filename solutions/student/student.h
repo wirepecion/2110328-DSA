ff028c2a5baac128ace2e3f72c316a891ff53c22
wirepecion <wirepecion44@gmail.com>
2024-11-13 14:51:40 +0700

    queue remove1

--- solutions/student/student.h
+++ solutions/student/student.h
@@ -2,12 +2,40 @@
 #define __STUDENT_H_
 
 #include <vector>
+#include <algorithm>
 #include "queue.h"
 
 template <typename T>
 void CP::queue<T>::remove_many(std::vector<size_t> pos)
-{
-  // your code here
-}
+  {
+  
+    if(pos.empty()) return;
+
+    std::sort(pos.begin(), pos.end());
+
+    // shiftleft store now how many data that was removed, this will use indexing for next remove
+    size_t shiftleft = 0;
+    size_t pos_idx = 0;
+
+    for (size_t i = 0; i < mSize; ++i)
+    {
+
+      size_t idx = (mFront + i) % mCap;
+      /* code */ 
+      if (pos_idx < pos.size() && i == pos[pos_idx])
+      {
+        /* code */
+        shiftleft++;
+        pos_idx++;
+      }else{
+        // shift data to left
+        mData[(mFront + i - shiftleft) % mCap] = mData[idx];
+      }
+      
+    }
+
+    mSize -= shiftleft;
+
+  }
 
 #endif
